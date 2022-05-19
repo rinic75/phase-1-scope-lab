3ed799e6c4117558a1c2327a3df938238226cc78
chang <cshin7374@gmail.com>
2022-05-19 12:53:57 -0500

    commit tree 31827fbe4372fe661676340aef8ffae3429bb91a

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+    customerName = 'BOB';
+}
+
+function setBestCustomer() {
+    window.bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer ='bob';
+
+function changeLeastFavoriteCustomer () {
+    leastFavoriteCustomer = 'not bob'
+}
